ff7c8f077e475700152b56baba2d408231547740
Marcus Martins <marcusjava89@gmail.com>
2025-06-26 15:43:25 -0300

    Adiciona mudanças no pom.xml, resources e etc.
    1 - Adicionamos a dependência para rodar o flyway e plugin para cor-
    rigir erro do parent no pom.xml.
    
    2 - Adicionamos comandos em application.properties para rodar flyway e
    colocamos none em ddl,auto para não gerar conflito com o flyway.
    
    3 - Criamos ambiente de produção e desenvolvimento (dev e prod).

--- src/test/java/loja/estudo/spring/LojaEstudoSpringApplicationTests.java
+++ src/test/java/loja/estudo/spring/LojaEstudoSpringApplicationTests.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -22,6 +23,7 @@ import loja.estudo.spring.controller.AcessoController;
 import loja.estudo.spring.model.Acesso;
 import loja.estudo.spring.repository.AcessoRepository;
 
+@Profile("test")
 @SpringBootTest(classes = LojaEstudoSpringApplication.class)
 public class LojaEstudoSpringApplicationTests extends TestCase{/*Biblioteca JUnit3 baixada.*/
 
